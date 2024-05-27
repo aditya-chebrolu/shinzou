4bd2a1abe61cb226d3c94d1adf8a10cdcbd4fc18
Aditya Chebrolu <thisisadityachebrolu@gmail.com>
2024-05-27 19:59:34 +0530

    Revert "cc"
    This reverts commit e0634cded48d32215069a700626b07cda7bed811.

---
+++ src/constants/data/cheat-sheets.ts
@@ -0,0 +1,14 @@
+export const cheatSheets = [
+  {
+    title: "Java Cheat sheet",
+    link: "https://github.com/aditya-chebrolu/cs-java",
+  },
+  {
+    title: "CSS Cheat sheet",
+    link: "https://github.com/aditya-chebrolu/cs-css",
+  },
+  {
+    title: "Mongo Cheat Sheet",
+    link: "https://github.com/aditya-chebrolu/cs-mongo",
+  },
+];
