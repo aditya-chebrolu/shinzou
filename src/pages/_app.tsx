dd6791dec7a35b063e4eb6a69ffd848370a2db59
Aditya Chebrolu <thisisadityachebrolu@gmail.com>
2024-07-31 23:08:29 +0530

    code changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,21 @@ import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { bunnyFont, codeFont, sourceSansPro } from "@styles/fonts";
 import ThemeProvider from "src/contexts/theme-provider";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main
-      className={`${sourceSansPro.className} ${codeFont.variable} ${bunnyFont.variable}`}
-    >
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </main>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main
+        className={`${sourceSansPro.className} ${codeFont.variable} ${bunnyFont.variable}`}
+      >
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </main>
+    </>
   );
 }
