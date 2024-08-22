a95a9b9152c3126ceb1530368e27ffe37f934ace
Aditya Chebrolu <thisisadityachebrolu@gmail.com>
2024-08-23 02:47:11 +0530

    code changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { css, keyframes } from "@emotion/react";
 import DeveloperResourcesCta from "@modules/home/dev-stuff";
 import PortfolioMeta from "@modules/home/meta";
-import { minWidth, noScrollbar, SvgContainer } from "@styles/index";
+import { minWidth, SvgContainer } from "@styles/index";
 import ExperienceSection from "@modules/home/exp";
 import SkillsSection from "@modules/home/skills-section";
 import { socials } from "@constants/socials";
@@ -17,127 +17,116 @@ const anim = keyframes`
 `;
 
 const styles = css`
-  /* min-height: 100dvh; */
-  background-image: var(--dotted-bg);
+  background: var(--dotted-bg);
   background-size: 17px 17px;
-  overflow: hidden;
+  min-height: 100dvh;
 
-  .wrapper {
-    display: grid;
-    grid-template-columns: 20px 1fr 20px;
-    grid-template-rows: repeat(5, max-content);
-    grid-template-areas:
-      "head head head"
-      ". exp ."
-      ". skills ."
-      "dev dev dev"
-      ". icons .";
-    row-gap: 30px;
+  border: solid 1px red;
+  display: grid;
+  grid-template-columns: 20px 1fr 20px;
+  grid-template-rows: repeat(4, max-content);
+  grid-template-areas:
+    "head head head"
+    ". exp ."
+    ". skills ."
+    "dev dev dev"
+    ". icons .";
+  row-gap: 30px;
 
-    overflow-x: hidden;
-    height: 100vh;
+  > div {
+    grid-column: 2/3;
+  }
 
-    ${noScrollbar}
+  #header {
+    grid-area: head;
+  }
 
-    > div {
-      grid-column: 2/3;
+  #exp {
+    grid-area: exp;
+  }
+
+  #skills {
+    grid-area: skills;
+  }
+
+  .resources {
+    grid-area: dev;
+    padding: 25px 20px;
+    position: relative;
+    overflow: hidden;
+
+    > .bg {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 200px;
+      background: url("pattern.png");
+      background-position: center;
+      background-repeat: repeat;
+      background-size: contain;
+      animation: ${anim} 10s linear infinite;
+
+      box-shadow: 0 20px 10px -20px rgba(0, 0, 0, 1) inset,
+        0 -20px 10px -20px rgba(0, 0, 0, 1) inset,
+        20px 0 10px -20px rgba(0, 0, 0, 1) inset;
     }
 
-    #header {
-      grid-area: head;
+    > .bg + div {
+      isolation: isolate;
     }
+  }
 
-    #exp {
-      grid-area: exp;
-    }
+  #icons {
+    display: flex;
+    margin-bottom: 30px;
+    gap: 30px;
+  }
 
-    #skills {
-      grid-area: skills;
-    }
+  ${minWidth()} {
+    grid-template-columns: 27% 1fr 27%;
+    row-gap: 50px;
+    padding-top: 0px;
 
     .resources {
-      grid-area: dev;
-      padding: 25px 20px;
-      position: relative;
-      overflow: hidden;
-
-      > .bg {
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 200px;
-        background: url("pattern.png");
-        background-position: center;
-        background-repeat: repeat;
-        background-size: contain;
-        animation: ${anim} 10s linear infinite;
-
-        box-shadow: 0 20px 10px -20px rgba(0, 0, 0, 1) inset,
-          0 -20px 10px -20px rgba(0, 0, 0, 1) inset,
-          20px 0 10px -20px rgba(0, 0, 0, 1) inset;
-      }
-
-      > .bg + div {
-        isolation: isolate;
-      }
+      padding: 25px 27%;
     }
 
-    #icons {
+    > .content {
       display: flex;
-      margin-bottom: 30px;
-      gap: 30px;
+      flex-direction: column;
+      gap: 10px;
     }
 
-    ${minWidth()} {
-      grid-template-columns: 27% 1fr 27%;
-      row-gap: 50px;
-      padding-top: 0px;
-
-      .resources {
-        padding: 25px 27%;
-      }
-
-      > .content {
-        /* grid-row: 2/3;
-        grid-column: 2/4; */
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-      }
-
-      > .icons {
-        gap: 40px;
-      }
+    > .icons {
+      gap: 40px;
     }
   }
 `;
 
 const Page = () => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const { scrollY } = useScroll({ container: containerRef });
+  const { scrollY } = useScroll();
   return (
     <>
       <PortfolioMeta />
-      <div css={styles}>
-        <div className="wrapper" ref={containerRef}>
-          <Header id="header" scrollY={scrollY} />
-          <ExperienceSection id="exp" />
-          <SkillsSection id="skills" />
-          <div className="resources">
-            <div className="bg" />
-            <DeveloperResourcesCta />
-          </div>
-          <div id="icons">
-            {socials.map(({ href, Icon }, idx) => (
-              <a href={href} key={idx}>
-                <SvgContainer h={30} fill="#2A3439">
-                  <Icon />
-                </SvgContainer>
-              </a>
-            ))}
-            <ResumeButton />
-          </div>
+      <div css={styles} ref={containerRef}>
+        <Header id="header" scrollY={scrollY} />
+        <ExperienceSection id="exp" />
+        <SkillsSection id="skills" />
+        <div className="resources">
+          <div className="bg" />
+          <DeveloperResourcesCta />
+        </div>
+        <div id="icons">
+          {socials.map(({ href, Icon }, idx) => (
+            <a href={href} key={idx}>
+              <SvgContainer h={30} fill="#2A3439">
+                <Icon />
+              </SvgContainer>
+            </a>
+          ))}
+          <ResumeButton />
         </div>
       </div>
     </>
